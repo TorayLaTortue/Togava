b748fb04a9a353a1f16a39633db8c4711720085e
TorayLaTortue <damien.pasquer3636@gmail.com>
2024-01-07 01:46:53 +0100

    avancement map

--- Jeu/Main2.java
+++ Jeu/Main2.java
@@ -27,37 +27,37 @@ import tableau.TableauMonstre;
 public class Main2 {
     public static void main(String[] args) {
 
+        Tableau c0 = new Tableau(0, 0).addBas().addGauche().addDroite(); //spawn
+
         Tableau[] tableaux = {
-            new Tableau(0, 0).addBas(),
-            new TableauMonstre(1, 0, new Monstre(TypeMonstre.BLOB)).addBas().addGauche(),
-            new Tableau(2, 0).addBas(),
-            new TableauMonstre(3, 0, new Monstre(TypeMonstre.DRAGON)).addBas().addDroite(),
+            new Tableau(1, 0).addHaut(),
+            new TableauMonstre(2, 0, new Monstre(TypeMonstre.BLOB)).addGauche().addHaut(),
+            new TableauMonstre(3, 0, new Monstre(TypeMonstre.DRAGON)).addDroite().addHaut(),
             new TableauMarchand(4, 0, new Marchand().addArme(Armes.CLAYMORE)).addHaut().addGauche(),
             new TableauCoffre(5, 0, Armes.EPEEPIERRE).addHaut().addDroite(),
-            new Tableau(6, 0),
-            new Tableau(7, 0),
-            new Tableau(8, 0),
-            new Tableau(9, 0),
-            
-            new TableauMonstre(0, 1, new Monstre(TypeMonstre.BLOB)).addBas().addGauche(),
-            new Tableau(1, 1).addBas(),
-            new Tableau(2, 1).addBas(),
-            new TableauMonstre(3, 1, new Monstre(TypeMonstre.DRAGON)).addBas().addDroite(),
+            new Tableau(6, 0).addHaut(),
+            new Tableau(7, 0).addHaut(),
+            new Tableau(8, 0).addHaut(),
+            new Tableau(9, 0).addHaut(),
+
+            new TableauMonstre(0, 1, new Monstre(TypeMonstre.BLOB)).addGauche(),
+            new Tableau(1, 1),
+            new Tableau(2, 1),
+            new TableauMonstre(3, 1, new Monstre(TypeMonstre.DRAGON)).addDroite(),
             new TableauMarchand(4, 1, new Marchand().addArme(Armes.CLAYMORE)).addHaut().addGauche(),
             new TableauCoffre(5, 1, Armes.EPEEPIERRE).addHaut().addDroite(),
             new Tableau(6, 1),
             new TableauMarchand(7, 1, new Marchand().addArme(Armes.CLAYMORE)),
             new Tableau(8, 1),
             new TableauCoffre(9, 1, Armes.EPEEPIERRE).addHaut().addDroite()
+
+            
         };
         
         
         
-
-
-        // Créez une image avec une taille fixe (par exemple, 800x600)
-        int width = 1000;
-        int height = 1000;
+        int width = 800;
+        int height = 800;
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
         // Obtenez un objet Graphics à partir de l'image
@@ -66,32 +66,50 @@ public class Main2 {
         for (Tableau tableau : tableaux) {
             drawTableau(g, tableau);
         }
+        drawTableau(g, c0);
 
+        g.dispose();// Libérez les ressources graphiques
+        showImage(image);// Affichez l'image dans une fenêtre
 
-        // Libérez les ressources graphiques
-        g.dispose();
-
-        // Affichez l'image dans une fenêtre
-        showImage(image);
+        ChoixPersonnage choixPersonnage = new ChoixPersonnage();
+        Joueur chevalier = choixPersonnage.choixPersonnage();
+		
+		c0.evenement();
     }
 
     private static void drawTableau(Graphics g, Tableau tableau) {
+
+        int x = tableau.getX() * 60;
+        int y = 600 + (-60 * tableau.getY());
+     
         // Dessinez le tableau sur l'image en fonction de son type
         if (tableau instanceof TableauMonstre) {
-            drawImage(g, "image/Monstre.png", (tableau.getX() * 60), (tableau.getY() * 60));
+            drawImage(g, "image/Monstre.png", x, y);
         } else if (tableau instanceof TableauMarchand) {
-            drawImage(g, "image/Marchand.png", (tableau.getX() * 60), (tableau.getY() * 60));
+            drawImage(g, "image/Marchand.png", x, y);
         } else if (tableau instanceof TableauCoffre) {
-            drawImage(g, "image/Coffre.png", (tableau.getX() * 60), (tableau.getY() * 60));
+            drawImage(g, "image/Coffre.png", x, y);
         } else if (tableau instanceof Tableau) {
-            drawImage(g, "image/PlaineVide.png", (tableau.getX() * 60), (tableau.getY() * 60));
-        }
-         else {
+            drawImage(g, "image/PlaineVide.png", x, y);
+        } else {
             // Dessinez une représentation par défaut pour les autres types de tableaux
             g.setColor(Color.BLACK);
-            g.drawRect((tableau.getX() * 60), (tableau.getY() * 60), 60, 60);
+            g.drawRect(x, y, 60, 60);
         }
-    }
+    
+        // Dessinez les indications de direction (haut, bas, gauche, droite)
+        if (tableau.hasHaut()) {
+            drawImage(g, "image/MurHaut.png", x, y);
+        }
+        if (tableau.hasBas()) {
+            drawImage(g, "image/MurBas.png", x, y-60);
+        }
+        if (tableau.hasGauche()) {
+            drawImage(g, "image/MurGauche.png", x, y);
+        }
+        if (tableau.hasDroite()) {
+            drawImage(g, "image/MurDroite.png", x+60, y);
+        }}
 
     private static void drawImage(Graphics g, String imageName, int x, int y) {
         // Chargez une image depuis un fichier
@@ -121,6 +139,8 @@ public class Main2 {
         frame.setVisible(true);
     }
 
+
+    
     
 }
 
